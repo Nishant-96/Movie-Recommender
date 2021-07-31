b5f2af712f4f390667913c04ff1115c1f4ffbf42
Nishant Pratihast <87017414+Nishant-96@users.noreply.github.com>
2021-07-31 16:04:20 +0530

    update: app Functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
+import React, { useState } from "react";
 import "./styles.css";
 
+var genres = ["Horror", "RomCom", "Thriller"];
+var movieList = {
+  Horror: [
+    "Conjuring: IMdb 7.5",
+    "Insidous: IMdb 6.8",
+    "It: IMdb 6.5",
+    "Annabelle: IMdb 5.9",
+    "Nun: IMdb 5.3"
+  ],
+  RomCom: [
+    "LaLa Land: IMdb 8.0",
+    "French Dispatch: IMdb 7.3",
+    "Yesterday: IMDb 6.8",
+    "Clueless: IMdb 6.8",
+    "Little Rascals: IMdb 6.3"
+  ],
+  Thriller: [
+    "Nobody: IMdb 7.4",
+    "Extraction: IMdb 6.7",
+    "Stillwater: IMdb 6.3",
+    "Snake Eyes: IMdb 5.7",
+    "Jolt: IMdb 5.7"
+  ]
+};
+
 export default function App() {
+  const [genreList, setGenre] = useState("");
+
+  function clickHandler(genre) {
+    var genreList = movieList[genre];
+    setGenre(genreList);
+  }
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="recommenderContainer">
+      <div className="App">
+        <h1 id="name">Movie Recommender</h1>
+        {genres.map(function (genre) {
+          return <button onClick={() => clickHandler(genre)}>{genre}</button>;
+        })}
+        <ul className="outputContainer">
+          <p>Top Five: </p>
+          <li>{genreList[0]}</li>
+          <li>{genreList[1]}</li>
+          <li>{genreList[2]}</li>
+          <li>{genreList[3]}</li>
+          <li>{genreList[4]}</li>
+        </ul>
+      </div>
     </div>
   );
 }
